ae3dbb979840ab8c5d3ed550a49ad6069d998840
AAnabioZZ <ars9000@gmail.com>
2025-03-27 21:35:59 +0300

    0011

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import subprocess
 
-# Запускаем основной бот из telegram_bot/bot.py
+# Запускаем основной бот из telegram_bot/bot.py fff
 if __name__ == "__main__":
     subprocess.run(["python", "telegram_bot/bot.py"])
